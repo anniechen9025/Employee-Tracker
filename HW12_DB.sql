cbff8789d04885ded5b52121ec34e0ec2a97229d
Jouan <77906989+anniechen9025@users.noreply.github.com>
2021-04-20 21:47:32 -0700

    set constructor

---
+++ HW12_DB.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS tracker_DB;
+CREATE DATABASE tracker_DB;
+USE tracker_DB;
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  category VARCHAR(45) NOT NULL,
+  salary DECIMAL(10,2) NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT DEFAULT 0,
+  PRIMARY KEY (id)
+);
